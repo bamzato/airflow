0f54e3a8e85e6eb816276af101ed49a176b70b86
bamzato <bamzato92.bp@gmail.com>
2024-04-18 12:21:35 +0700

    Initial Trial

---
+++ dags/dags_instance/dag_initial.py
@@ -0,0 +1,52 @@
+from airflow import DAG
+from datetime import datetime
+
+# Operator
+from airflow.operators.python_operator import PythonOperator
+
+#Define default arguments
+default_args = {
+ 'owner': 'airflow',
+ 'start_date': datetime (2023, 4, 5)
+}
+
+# Instantiate your DAG
+# https://airflow.apache.org/docs/apache-airflow/1.10.1/scheduler.html
+period = '*/1 * * * *'
+# once = '@once'
+# daily = '@daily'
+dag = DAG ('my_first_dag', default_args=default_args, schedule_interval=period)
+
+# Define tasks
+def task1():
+    print ("Melakukan task 1")
+    return "Task 1"
+
+def task2():
+    print ("Executing Task 2")
+    return "Task 2"
+
+def task3():
+    print ("Executing Task 3 - Err")
+    print(b)
+    return b
+
+
+task_1 = PythonOperator(
+    task_id='task_1',
+    python_callable=task1,
+    dag=dag
+)
+task_2 = PythonOperator(
+    task_id='task_2',
+    python_callable=task2,
+    dag=dag
+)
+task_3_fail = PythonOperator(
+    task_id='task_3_fail',
+    python_callable=task3,
+    dag=dag
+)
+
+# Set task dependencies
+task_1 >> task_2 >> task_3_fail
